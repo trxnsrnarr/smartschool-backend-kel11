b892c02313c0fda6e9e4a564281b1eb4ed0f5862
Rochim <rochim.rci26@gmail.com>
2021-04-19 14:20:30 +0700

    change belongsToMany to belongsTo on proyek

--- app/Models/MAnggotaProyek.js
+++ app/Models/MAnggotaProyek.js
@@ -1,22 +1,22 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MAnggotaProyek extends Model {
-    static get table() {
-        return "m_anggota_proyek";
-      }
-    
-      user() {
-        return this.belongsTo("App/Models/User", "m_user_id");
-      }
-      proyek(){
-          return this.belongsToMany("App/Models/MProyek");
-      }
-      role(){
-        return this.belongsTo("App/Models/MAnggotaProyekRole");
-      }
+  static get table() {
+    return "m_anggota_proyek";
+  }
+
+  user() {
+    return this.belongsTo("App/Models/User", "m_user_id");
+  }
+  proyek() {
+    return this.belongsTo("App/Models/MProyek");
+  }
+  role() {
+    return this.belongsTo("App/Models/MAnggotaProyekRole");
+  }
 }
 
-module.exports = MAnggotaProyek
+module.exports = MAnggotaProyek;
