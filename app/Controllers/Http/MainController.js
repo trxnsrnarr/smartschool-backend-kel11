fc4ad54e1087cf23d8902947f01527a838f78f10
Rochim <rochim.rci26@gmail.com>
2021-04-19 12:48:23 +0700

    update absen guru

--- app/Controllers/Http/MainController.js
+++ app/Controllers/Http/MainController.js
@@ -5823,7 +5823,7 @@ class MainController {
 
       await Promise.all(
         absen.toJSON().map(async (d) => {
-          worksheet.getRow(10).values = [
+          worksheet.getRow(11).values = [
             "Nama",
             "Absen",
             "Keterangan",
