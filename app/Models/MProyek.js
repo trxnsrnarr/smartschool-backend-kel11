31c1650d29cfa8f437a61cc5875d2d3b61256ba6
Rochim <rochim.rci26@gmail.com>
2021-04-19 14:20:53 +0700

    refactoring

--- app/Models/MProyek.js
+++ app/Models/MProyek.js
@@ -1,21 +1,21 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
-class MProyek extends Model { 
-static get table() {
+class MProyek extends Model {
+  static get table() {
     return "m_proyek";
   }
-user() {
-   return this.belongsTo("App/Models/User", "m_user_id");
-}
-sekolah() {
+  user() {
+    return this.belongsTo("App/Models/User", "m_user_id");
+  }
+  sekolah() {
     return this.belongsTo("App/Models/MSekolah");
   }
-status() {
+  status() {
     return this.belongsTo("App/Models/MStatusProyek");
   }
 }
 
-module.exports = MProyek
+module.exports = MProyek;
