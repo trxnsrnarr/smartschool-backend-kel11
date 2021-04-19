4a8888c4efc197486ebda99580062a957f0e57f5
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-04-19 13:21:00 +0700

    Menambahkan relasi model proyek

--- app/Models/MProyek.js
+++ app/Models/MProyek.js
@@ -3,7 +3,18 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class MProyek extends Model {
+class MProyek extends Model { static get table() {
+    return "m_proyek";
+  }
+user() {
+   return this.belongsTo("App/Models/User", "m_user_id");
+}
+sekolah() {
+    return this.belongsTo("App/Models/MSekolah");
+  }
+status() {
+    return this.belongsTo("App/Models/MStatusProyek");
+  }
 }
 
 module.exports = MProyek
