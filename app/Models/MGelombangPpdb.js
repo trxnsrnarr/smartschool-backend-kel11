b9fd2e39f99afac2b7c70237ad7ed25586be70ce
Rochim <rochim.rci26@gmail.com>
2021-04-21 20:42:45 +0700

    gelombang ppdb models

--- app/Models/MGelombangPpdb.js
+++ app/Models/MGelombangPpdb.js
@@ -3,9 +3,30 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
 
+const moment = require("moment");
+require("moment/locale/id");
+moment.locale("id");
+
 class MGelombangPpdb extends Model {
   static get table() {
     return "m_gelombang_ppdb";
+  }
+
+  static get computed() {
+    return ["keterangan"];
+  }
+
+  getKeterangan({ dibuka, ditutup }) {
+    if (
+      moment().format("YYYY-MM-DD HH:mm:ss") <
+      moment(dibuka).format("YYYY-MM-DD HH:mm:ss")
+    ) {
+      return "Akan Dibuka";
+    } else if (moment(new Date()).isBetween(dibuka, ditutup)) {
+      return "Dibuka";
+    } else {
+      return "Sudah Ditutup";
+    }
   }
 }
 
