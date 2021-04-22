1ab1773584de03083d009287a70f1bb34b24df64
Rochim <rochim.rci26@gmail.com>
2021-04-22 09:51:40 +0700

    profil migration

---
+++ database/migrations/1619049422029_m_profil_user_schema.js
@@ -0,0 +1,84 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class MProfilUserSchema extends Schema {
+  up() {
+    this.create("m_profil_user", (table) => {
+      table.increments();
+      // alamat
+      table.text("alamat");
+      table.string("provinces_id", 2).index("provinces_id");
+      table.string("regencies_id", 4).index("regencies_id");
+      table.string("districts_id", 7).index("districts_id");
+      table.string("villages_id", 10).index("villages_id");
+      table.string("kode_pos");
+
+      // kesehatan
+      table.integer("tb"); // tinggi badan
+      table.integer("bb"); // berat badan
+      table.string("gol_darah");
+      table.boolean("buta_warna").defaultTo(0);
+      table.string("kacamata");
+      table.string("disabilitas");
+      table.text("surat_keterangan_sehat");
+      table.text("surat_keterangan_buta_warna");
+
+      // ortu
+      table.string("nama_ayah");
+      table.string("telp_ayah");
+      table.text("alamat_ayah");
+      table.string("nama_ibu");
+      table.string("telp_ibu");
+      table.text("alamat_ibu");
+      table.integer("m_user_id").unsigned().index("m_user_id");
+      table
+        .foreign("m_user_id")
+        .references("m_user.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+
+      // rapor
+      table.integer("fisika1");
+      table.integer("fisika2");
+      table.integer("fisika3");
+      table.integer("fisika4");
+      table.integer("fisika5");
+      table.integer("fisika6");
+      table.integer("matematika1");
+      table.integer("matematika2");
+      table.integer("matematika3");
+      table.integer("matematika4");
+      table.integer("matematika5");
+      table.integer("matematika6");
+      table.integer("bindo1");
+      table.integer("bindo2");
+      table.integer("bindo3");
+      table.integer("bindo4");
+      table.integer("bindo5");
+      table.integer("bindo6");
+      table.integer("bing1");
+      table.integer("bing2");
+      table.integer("bing3");
+      table.integer("bing4");
+      table.integer("bing5");
+      table.integer("bing6");
+
+      // lampiran rapor
+      table.text("semester1");
+      table.text("semester2");
+      table.text("semester3");
+      table.text("semester4");
+      table.text("semester5");
+      table.text("semester6");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("m_profil_user");
+  }
+}
+
+module.exports = MProfilUserSchema;
