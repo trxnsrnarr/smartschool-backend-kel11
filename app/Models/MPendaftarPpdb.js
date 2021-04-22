6debc6d19a7fbea25eeddc2f88fa589bc9ecc418
Rochim <rochim.rci26@gmail.com>
2021-04-22 09:51:01 +0700

    model pendaftar ppdb

---
+++ app/Models/MPendaftarPpdb.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use("Model");
+
+class MPendaftarPpdb extends Model {
+  static get table() {
+    return "m_pendaftar_ppdb";
+  }
+}
+
+module.exports = MPendaftarPpdb;
