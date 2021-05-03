90b8cb34514b0be94e5bd0a8160e24c388d7dd30
Rochim <rochim.rci26@gmail.com>
2021-05-03 08:50:12 +0700

    add relation in rpp model

--- app/Models/MRpp.js
+++ app/Models/MRpp.js
@@ -7,6 +7,18 @@ class MRpp extends Model {
   static get table() {
     return "m_rpp";
   }
+
+  mataPelajaran() {
+    return this.belongsTo("App/Models/MMataPelajaran");
+  }
+
+  sekolah() {
+    return this.belongsTo("App/Models/MSekolah");
+  }
+
+  user() {
+    return this.belongsTo("App/Models/User", "m_user_id");
+  }
 }
 
 module.exports = MRpp;
