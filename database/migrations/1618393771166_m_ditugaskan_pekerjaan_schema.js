4a9ba6209105d14bedbb4f6ee08014effac5a1c8
Rochim <rochim.rci26@gmail.com>
2021-05-09 14:41:38 +0700

    change to user id

--- database/migrations/1618393771166_m_ditugaskan_pekerjaan_schema.js
+++ database/migrations/1618393771166_m_ditugaskan_pekerjaan_schema.js
@@ -1,32 +1,35 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class MDitugaskanPekerjaanSchema extends Schema {
-  up () {
-    this.create('m_ditugaskan_pekerjaan', (table) => {
-      table.increments()
-      table.integer("m_pekerjaan_proyek_id").unsigned().index('m_pekerjaan_proyek_id');
+  up() {
+    this.create("m_ditugaskan_pekerjaan", (table) => {
+      table.increments();
+      table
+        .integer("m_pekerjaan_proyek_id")
+        .unsigned()
+        .index("m_pekerjaan_proyek_id");
       table
         .foreign("m_pekerjaan_proyek_id")
         .references("m_pekerjaan_proyek.id")
         .onDelete("cascade")
         .onUpdate("cascade");
-      table.integer("m_anggota_proyek_id").unsigned().index('m_anggota_proyek_id');
+      table.integer("m_user_id").unsigned().index("m_user_id");
       table
-        .foreign("m_anggota_proyek_id")
-        .references("m_anggota_proyek.id")
+        .foreign("m_user_id")
+        .references("m_user.id")
         .onDelete("cascade")
         .onUpdate("cascade");
-      table.boolean("dihapus")
-      table.timestamps()
-    })
+      table.boolean("dihapus");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('m_ditugaskan_pekerjaan')
+  down() {
+    this.drop("m_ditugaskan_pekerjaan");
   }
 }
 
-module.exports = MDitugaskanPekerjaanSchema
+module.exports = MDitugaskanPekerjaanSchema;
