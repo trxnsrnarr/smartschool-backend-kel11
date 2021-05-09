aeeed4f5e33b744c20de787186a74d576ee034bb
Rochim <rochim.rci26@gmail.com>
2021-05-09 14:40:56 +0700

    add relation pekerjaan

--- app/Models/MKategoriPekerjaan.js
+++ app/Models/MKategoriPekerjaan.js
@@ -1,12 +1,16 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MKategoriPekerjaan extends Model {
-    static get table() {
-        return "m_kategori_pekerjaan";
-      }
+  static get table() {
+    return "m_kategori_pekerjaan";
+  }
+
+  pekerjaan() {
+    return this.hasMany("App/Models/MPekerjaanProyek");
+  }
 }
 
-module.exports = MKategoriPekerjaan
+module.exports = MKategoriPekerjaan;
