8345b09efd3b99e120b6ea71326e79f53423b130
Rochim <rochim.rci26@gmail.com>
2021-05-09 14:40:50 +0700

    add relation

--- app/Models/MDitugaskanPekerjaan.js
+++ app/Models/MDitugaskanPekerjaan.js
@@ -1,12 +1,16 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MDitugaskanPekerjaan extends Model {
-    static get table() {
-        return "m_ditugaskan_pekerjaan"
-    }
+  static get table() {
+    return "m_ditugaskan_pekerjaan";
+  }
+
+  user() {
+    return this.belongsTo("App/Models/User", "m_user_id");
+  }
 }
 
-module.exports = MDitugaskanPekerjaan
+module.exports = MDitugaskanPekerjaan;
