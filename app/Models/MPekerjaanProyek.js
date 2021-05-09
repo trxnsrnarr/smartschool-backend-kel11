e4e06574947108b004268730ce121d712576c4e9
Rochim <rochim.rci26@gmail.com>
2021-05-09 14:41:04 +0700

    add relation ditugaskan

--- app/Models/MPekerjaanProyek.js
+++ app/Models/MPekerjaanProyek.js
@@ -1,12 +1,16 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MPekerjaanProyek extends Model {
-    static get table() {
-        return "m_pekerjaan_proyek";
-      }
+  static get table() {
+    return "m_pekerjaan_proyek";
+  }
+
+  ditugaskan() {
+    return this.hasMany("App/Models/MDitugaskanPekerjaan");
+  }
 }
 
-module.exports = MPekerjaanProyek
+module.exports = MPekerjaanProyek;
