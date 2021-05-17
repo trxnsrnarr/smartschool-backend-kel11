b113d92561e953b2cf44ef52fd945d2446ed9d98
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-05-17 16:33:44 +0700

    fix TkRekapNilai

--- app/Models/TkRekapNilai.js
+++ app/Models/TkRekapNilai.js
@@ -1,9 +1,18 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class TkRekapNilai extends Model {
+  static get table() {
+    return "tk_rekap_nilai";
+  }
+  user() {
+    return this.belongsTo("App/Models/User", "m_user_id");
+  }
+  Rombel() {
+    return this.hasMany("App/Models/MRombel");
+  }
 }
 
-module.exports = TkRekapNilai
+module.exports = TkRekapNilai;
