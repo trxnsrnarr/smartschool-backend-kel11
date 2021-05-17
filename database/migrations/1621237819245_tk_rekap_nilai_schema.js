30e6699d1f7b4f35bc773733566925cc69929815
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-05-17 15:01:16 +0700

    update tk_rekap_nilai

--- database/migrations/1621237819245_tk_rekap_nilai_schema.js
+++ database/migrations/1621237819245_tk_rekap_nilai_schema.js
@@ -1,19 +1,38 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class TkRekapNilaiSchema extends Schema {
-  up () {
-    this.create('tk_rekap_nilais', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("tk_rekap_nilai", (table) => {
+      table.increments();
+      table.integer("m_user_id").unsigned().index("m_user_id");
+      table
+        .foreign("m_user_id")
+        .references("m_user.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.integer("nilai");
+      table.integer("m_rekap_id").unsigned().index("m_rekap_id");
+      table
+        .foreign("m_rekap_id")
+        .references("m_rekap.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.integer("m_rombel_id").unsigned().index("m_rombel_id");
+      table
+        .foreign("m_rombel_id")
+        .references("m_rombel.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('tk_rekap_nilais')
+  down() {
+    this.drop("tk_rekap_nilai");
   }
 }
 
-module.exports = TkRekapNilaiSchema
+module.exports = TkRekapNilaiSchema;
