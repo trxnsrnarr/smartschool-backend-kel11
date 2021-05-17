7b720a5e7e690f09e244562abcb365fd936160e5
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-05-17 14:58:34 +0700

    update m_rekap

--- database/migrations/1621237810260_m_rekap_schema.js
+++ database/migrations/1621237810260_m_rekap_schema.js
@@ -1,19 +1,39 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class MRekapSchema extends Schema {
-  up () {
-    this.create('m_rekaps', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("m_rekap", (table) => {
+      table.increments();
+      table.boolean("di_ss");
+      table.string("judul");
+      table.text("deskripsi");
+      table.dateTime("tanggal");
+      table.integer("kkm");
+      table.integer("dibawahkkm");
+      table.string("teknik");
+      table.integer("m_materi_id").unsigned().index("m_materi_id");
+      table
+        .foreign("m_materi_id")
+        .references("m_materi.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.integer("m_tugas_id").unsigned().index("m_tugas_id");
+      table
+        .foreign("m_tugas_id")
+        .references("m_tugas.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.boolean("dihapus");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('m_rekaps')
+  down() {
+    this.drop("m_rekap");
   }
 }
 
-module.exports = MRekapSchema
+module.exports = MRekapSchema;
