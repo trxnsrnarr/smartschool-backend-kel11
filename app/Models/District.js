190f608eb99539422fb5ce2e39237f3cca4f785f
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-06-01 17:30:05 +0700

    tambah relasi ke regency

--- app/Models/District.js
+++ app/Models/District.js
@@ -1,9 +1,12 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class District extends Model {
+  regency() {
+    return this.belongsTo("App/Models/Regency");
+  }
 }
 
-module.exports = District
+module.exports = District;
