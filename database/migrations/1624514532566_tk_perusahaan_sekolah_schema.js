70b9601b5a620f98ee1cbc00020d5e772ecae362
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-06-24 13:45:03 +0700

    tambah migrasi tk_perusahaan_sekolah

---
+++ database/migrations/1624514532566_tk_perusahaan_sekolah_schema.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class TkPerusahaanSekolahSchema extends Schema {
+  up() {
+    this.create("tk_perusahaan_sekolahs", (table) => {
+      table.increments();
+      table.integer("m_perusahaan_id").unsigned().index("m_perusahaan_id");
+      table
+        .foreign("m_perusahaan_id")
+        .references("m_perusahaan.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.integer("m_sekolah_id").unsigned().index("m_sekolah_id");
+      table
+        .foreign("m_sekolah_id")
+        .references("m_sekolah.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.string("status");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("tk_perusahaan_sekolahs");
+  }
+}
+
+module.exports = TkPerusahaanSekolahSchema;
