f2734293179f9e3868e70b1177c7dd3457e258be
ShiroXRai <51844770+ShiroXRai@users.noreply.github.com>
2021-06-28 14:55:58 +0700

    a

--- database/migrations/1624861763528_m_kotak_masuk_komen_schema.js
+++ database/migrations/1624861763528_m_kotak_masuk_komen_schema.js
@@ -1,19 +1,33 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class MKotakMasukKomenSchema extends Schema {
-  up () {
-    this.create('m_kotak_masuk_komen', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("m_kotak_masuk_komen", (table) => {
+      table.increments();
+      table.text("komen");
+      table.boolean("dihapus").defaultTo(0);
+      table.integer("m_kotan_masuk_id").unsigned().index("m_kotan_masuk_id");
+      table
+        .foreign("m_kotan_masuk_id")
+        .references("m_kotan_masuk.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.integer("m_user_id").unsigned().index("m_user_id");
+      table
+        .foreign("m_user_id")
+        .references("m_user.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('m_kotak_masuk_komen')
+  down() {
+    this.drop("m_kotak_masuk_komen");
   }
 }
 
-module.exports = MKotakMasukKomenSchema
+module.exports = MKotakMasukKomenSchema;
