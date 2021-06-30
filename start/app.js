ffad4d348a057d578fdbebed2fdcecaedaa8633c
shiroxros <raihanvans@gmail.com>
2021-06-30 12:34:06 +0700

    tambah mail

--- start/app.js
+++ start/app.js
@@ -18,6 +18,7 @@ const providers = [
   "@adonisjs/lucid/providers/LucidProvider",
   "@adonisjs/validator/providers/ValidatorProvider",
   "@adonisjs/drive/providers/DriveProvider",
+  "@adonisjs/mail/providers/MailProvider",
 ];
 
 /*
