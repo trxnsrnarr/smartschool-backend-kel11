bf3f02eed694b89dab997c2c870a52265e4222ea
shiroxros <raihanvans@gmail.com>
2021-07-13 19:35:14 +0700

    tambah relasi ke TkSiswaPelanggaran

--- app/Models/MPelanggaran.js
+++ app/Models/MPelanggaran.js
@@ -1,16 +1,20 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MPelanggaran extends Model {
-    static get table() {
-        return "m_pelanggaran";
-      }
-    
-      kategori() {
-        return this.belongsTo("App/Models/MKategoriPelanggaran");
-      }
+  static get table() {
+    return "m_pelanggaran";
+  }
+
+  kategori() {
+    return this.belongsTo("App/Models/MKategoriPelanggaran");
+  }
+
+  siswa() {
+    return this.hasMany("App/Models/TkSiswaPelanggaran");
+  }
 }
 
-module.exports = MPelanggaran
+module.exports = MPelanggaran;
