971a89bd051ba7fcaedc7f1de22b5cef6bfe9b35
shiroxros <raihanvans@gmail.com>
2021-07-17 20:38:39 +0700

    fix

--- app/Models/MAlumni.js
+++ app/Models/MAlumni.js
@@ -8,7 +8,6 @@ class MAlumni extends Model {
   static get table() {
     return "m_alumni";
   }
-  // ini masih buffer
   getDeskripsi(deskripsi) {
     return deskripsi ? htmlEscaper.unescape(pertanyaan) : "";
   }
