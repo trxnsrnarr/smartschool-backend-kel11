155c13a04886b2daf0c5772c3552d31bb04ded91
gideonfrans22 <gideonfrans22@gmail.com>
2021-08-09 16:07:59 +0700

    update pm2 deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: {
     name: "cd-adonis",
     script: "npm",
-    args: "run pm2",
+    args: "run pm2 --env production",
     instances: 1,
     exec_mode: "cluster",
     env_production: {
