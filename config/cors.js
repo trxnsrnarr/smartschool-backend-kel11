0f74891eb8dc412f346d6d678b1b6c716a07e31a
Rochim <rochim.rci26@gmail.com>
2021-08-13 16:04:16 +0700

    coba fix cors

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: "*",
+  origin: ["*", "http://smartschool.smkn6tangsel.sch.id/smartschool"],
 
   /*
   |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ module.exports = {
   | Function - Receives the current header and should return one of the above values.
   |
   */
-  headers: "*",
+  headers: ["*", "http://smartschool.smkn6tangsel.sch.id/smartschool"],
 
   /*
   |--------------------------------------------------------------------------
