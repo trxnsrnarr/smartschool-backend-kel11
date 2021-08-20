68d99cba46b80a19b87a4287f086a0009f0f7926
ShiroXRai <raihanvans@gmail.com>
2021-08-20 15:09:26 +0700

    tambah relasi baru

--- app/Models/MMataPelajaran.js
+++ app/Models/MMataPelajaran.js
@@ -15,6 +15,10 @@ class MMataPelajaran extends Model {
   user() {
     return this.belongsTo("App/Models/User", "m_user_id");
   }
+
+  nilaiSiswa() {
+    return this.hasMany("App/Models/MUjianSiswa", "id", "m_mata_pelajaran_id");
+  }
 }
 
 module.exports = MMataPelajaran;
