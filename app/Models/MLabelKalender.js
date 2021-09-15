c5396047b6fcbffd74964e108122b9654105158d
ShiroXRai <raihanvans@gmail.com>
2021-09-15 16:33:29 +0700

    f

--- app/Models/MLabelKalender.js
+++ app/Models/MLabelKalender.js
@@ -1,12 +1,12 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MLabelKalender extends Model {
-    static get table() {
-        return "m_kegiatan_kalender";
-      }
+  static get table() {
+    return "m_label_kalender";
+  }
 }
 
-module.exports = MLabelKalender
+module.exports = MLabelKalender;
