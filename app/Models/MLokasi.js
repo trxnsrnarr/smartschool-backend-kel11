ced3f5a76a97564846efdde6644e52c29a9c484f
gideonfrans22 <gideonfrans22@gmail.com>
2021-09-16 15:22:00 +0700

    update models

--- app/Models/MLokasi.js
+++ app/Models/MLokasi.js
@@ -1,20 +1,20 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class MLokasi extends Model {
-    static get table() {
-        return "m_lokasi";
-      }
+  static get table() {
+    return "m_lokasi";
+  }
 
-      barang() {
-        return this.hasMany("App/Models/MBarang");
-      }
+  barang() {
+    return this.hasMany("App/Models/MBarang");
+  }
 
-      getFoto(foto) {
-        return foto ? foto.split(",") : [];
-      }
+  getFoto(foto) {
+    return foto ? foto.split(",") : [];
+  }
 }
 
-module.exports = MLokasi
+module.exports = MLokasi;
