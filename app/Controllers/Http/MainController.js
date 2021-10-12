fd4b661e661ce4a704ec689b683a8e0582015fa6
gideonfrans22 <gideonfrans22@gmail.com>
2021-10-12 12:54:20 +0700

    link foto masuk camera

--- app/Controllers/Http/MainController.js
+++ app/Controllers/Http/MainController.js
@@ -8499,7 +8499,10 @@ class MainController {
                   foto_masuk_cam: anggota.user
                     ? anggota.user.absen
                       ? anggota.user.absen.length
-                        ? "https://server1.smarteschool.net/camera/" + anggota.user.absen[0].foto_masuk_local
+                        ? anggota.user.absen[0].foto_masuk_local
+                          ? "https://server1.smarteschool.net/camera/" +
+                            anggota.user.absen[0].foto_masuk_local
+                          : "-"
                         : "-"
                       : "-"
                     : "-",
@@ -8513,7 +8516,10 @@ class MainController {
                   foto_pulang_cam: anggota.user
                     ? anggota.user.absen
                       ? anggota.user.absen.length
-                        ? "https://server1.smarteschool.net/camera/" + anggota.user.absen[0].foto_pulang_local
+                        ? anggota.user.absen[0].foto_pulang_local
+                          ? "https://server1.smarteschool.net/camera/" +
+                            anggota.user.absen[0].foto_pulang_local
+                          : "-"
                         : "-"
                       : "-"
                     : "-",
