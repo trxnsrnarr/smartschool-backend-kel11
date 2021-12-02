2331b5612e208f316b09aa35123e8d51be8fc551
gideonfrans22 <gideonfrans22@gmail.com>
2021-12-02 15:47:15 +0700

    download lunas pembayaran update

--- app/Controllers/Http/MainController.js
+++ app/Controllers/Http/MainController.js
@@ -40158,7 +40158,7 @@ class MainController {
       //   })
       // })
       .with("siswa", (builder) => {
-        builder.with("user", (builder) => {
+        builder.where({ dihapus: 0 }).with("user", (builder) => {
           builder.select("id", "whatsapp", "nama");
         });
       })
