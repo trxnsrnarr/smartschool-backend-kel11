c2aa06dc01cb7e606540bb0e39209a973ff43a06
ShiroXRai <raihanvans@gmail.com>
2021-12-13 18:32:36 +0700

    fix

--- app/Controllers/Http/MainController.js
+++ app/Controllers/Http/MainController.js
@@ -4143,7 +4143,7 @@ class MainController {
                   builder.where({ dihapus: 0 });
                 })
                 .with("rekapSikap", (builder) => {
-                  builder.with("predikat").where({ dihapus: 0 }).andWhere({m_mata_pelajaran: data.m_mata_pelajaran_id});
+                  builder.with("predikat").where({ dihapus: 0 }).andWhere({m_mata_pelajaran_id: data.m_mata_pelajaran_id});
                 })
                 .with("nilaiUjian", (builder) => {
                   if (data) {
@@ -4226,7 +4226,7 @@ class MainController {
                     builder.where({ dihapus: 0 });
                   })
                   .with("rekapSikap", (builder) => {
-                    builder.with("predikat").where({ dihapus: 0 }).andWhere({m_mata_pelajaran: data.m_mata_pelajaran_id});
+                    builder.with("predikat").where({ dihapus: 0 }).andWhere({m_mata_pelajaran_id: data.m_mata_pelajaran_id});
                   })
                   .with("nilaiUjian", (builder) => {
                     builder.where({
