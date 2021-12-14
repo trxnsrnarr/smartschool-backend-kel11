58cad2d42f12bfb187407702b449511c8774ec7f
gideonfrans22 <gideonfrans22@gmail.com>
2021-12-14 17:48:25 +0700

    relasi

--- app/Models/MRaporEkskul.js
+++ app/Models/MRaporEkskul.js
@@ -7,6 +7,9 @@ class MRaporEkskul extends Model {
   static get table() {
     return "m_rapor_ekskul";
   }
+  ekskul() {
+    return this.belongsTo("App/Models/MEkstrakurikuler");
+  }
   rombel() {
     return this.belongsTo("App/Models/MRombel");
   }
