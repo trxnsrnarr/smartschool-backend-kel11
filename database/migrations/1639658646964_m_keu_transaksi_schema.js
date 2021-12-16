d6e314550ee4cc9640f1833e025fdfa6d570e8e4
ShiroXRai <raihanvans@gmail.com>
2021-12-16 20:46:34 +0700

    update

--- database/migrations/1639658646964_m_keu_transaksi_schema.js
+++ database/migrations/1639658646964_m_keu_transaksi_schema.js
@@ -7,6 +7,15 @@ class MKeuTransaksiSchema extends Schema {
   up() {
     this.create("m_keu_transaksi", (table) => {
       table.increments();
+      table.string("nama");
+      table.date("tanggal");
+      table.string("nomor");
+      table.integer("m_sekolah_id").unsigned().index("m_sekolah_id");
+      table
+        .foreign("m_sekolah_id")
+        .references("m_sekolah.id")
+        .onDelete("cascade")
+        .onUpdate("cascade");
       table.timestamps();
     });
   }
