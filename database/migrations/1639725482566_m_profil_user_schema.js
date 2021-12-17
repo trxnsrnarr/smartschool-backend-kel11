8ec2181d510e207221d1957b5afcc986e49dd86b
ShiroXRai <raihanvans@gmail.com>
2021-12-17 14:19:58 +0700

    update Mprofiluser

---
+++ database/migrations/1639725482566_m_profil_user_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class MProfilUserSchema extends Schema {
+  up () {
+    this.table('m_profil_user', (table) => {
+      table.string("no_ijazah");
+      table.string("tahun_ijazah");
+      table.text("file_ijazah");
+      // alter table
+    })
+  }
+
+  down () {
+    this.table('m_profil_user', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = MProfilUserSchema
