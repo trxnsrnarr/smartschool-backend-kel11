bd96ef0ff98a4a6407feb2a83760f42a8101544e
gideonfrans22 <gideonfrans22@gmail.com>
2021-12-21 17:54:34 +0700

    update table m_sekolah

---
+++ database/migrations/1640083988847_update_m_sekolah_schema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class UpdateMSekolahSchema extends Schema {
+  up() {
+    this.table("m_sekolah", (table) => {
+      // alter table
+      table.string("link_rapor");
+      table.string("link_dapodik");
+    });
+  }
+
+  down() {
+    this.table("m_sekolah", (table) => {
+      table.dropColumn("link_rapor");
+      table.dropColumn("link_dapodik");
+      // reverse alternations
+    });
+  }
+}
+
+module.exports = UpdateMSekolahSchema;
