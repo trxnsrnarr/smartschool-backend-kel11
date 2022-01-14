f6b454d1a77880e043061d0c04f5bdc43654fb39
gideonfrans22 <gideonfrans22@gmail.com>
2022-01-14 14:41:20 +0700

    status aktif

--- app/Models/MRencanaKeuangan.js
+++ app/Models/MRencanaKeuangan.js
@@ -7,6 +7,13 @@ class MRencanaKeuangan extends Model {
   static get table() {
     return "m_rencana_keuangan";
   }
+
+  setAktif({ tanggal_akhir, tanggal_awal }) {
+    return (
+      tanggal_akhir >= moment().format("YYYY-MM-DD") &&
+      tanggal_awal <= moment().format("YYYY-MM-DD")
+    );
+  }
 }
 
 module.exports = MRencanaKeuangan;
