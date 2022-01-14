157da529ad554ea431c31d6fa9f85bcd9413f273
gideonfrans22 <gideonfrans22@gmail.com>
2022-01-14 12:11:52 +0700

    controller keuangan

---
+++ app/Controllers/Http/KeuanganController.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const MSekolah = use("App/Models/MSekolah");
+const Mta = use("App/Models/Mta");
+const User = use("App/Models/User");
+
+class KeuanganController {
+  async getSekolahByDomain(domain) {
+    const sekolah = await MSekolah.query()
+      .with("informasi")
+      .where("domain", "like", `%${domain}%`)
+      .first();
+
+    if (!sekolah) {
+      return "404";
+    }
+
+    return sekolah;
+  }
+
+  async getTAAktif(sekolah) {
+    const ta = await Mta.query()
+      .select("id", "tahun")
+      .where({ m_sekolah_id: sekolah.id })
+      .andWhere({ aktif: 1 })
+      .andWhere({ dihapus: 0 })
+      .first();
+
+    if (!ta) {
+      return "404";
+    }
+
+    return ta;
+  }
+
+  async getRencanaKeuangan({ request, response, auth }) {
+    const domain = request.headers().origin;
+
+    const sekolah = await this.getSekolahByDomain(domain);
+
+    if (sekolah == "404") {
+      return response.notFound({ message: "Sekolah belum terdaftar" });
+    }
+
+    const user = await auth.getUser();
+  }
+}
+
+module.exports = KeuanganController;
