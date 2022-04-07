a8fd9f76b10a3511011c7a57cacb103cd1f85bdf
ShiroXRai <raihanvans@gmail.com>
2022-04-07 11:51:25 +0700

    get mulai

--- app/Models/MPerusahaan.js
+++ app/Models/MPerusahaan.js
@@ -25,6 +25,18 @@ class MPerusahaan extends Model {
       "id",
       "m_perusahaan_id"
   }
+
+  getMulaiKontrak(mulai_kontrak) {
+    return mulai_kontrak
+      ? moment(mulai_kontrak).format("YYYY-MM-DD")
+      : null;
+  }
+
+  getAkhirKontrak(akhir_kontrak) {
+    return akhir_kontrak
+      ? moment(akhir_kontrak).format("YYYY-MM-DD")
+      : null;
+  }
 }
 
 module.exports = MPerusahaan;
