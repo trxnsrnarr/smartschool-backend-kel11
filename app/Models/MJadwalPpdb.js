c700e1a1f9cfc319171b516649a97bb61be0b26f
ihsannur <ihsannurraihan@gmail.com>
2022-06-17 15:40:16 +0700

    add htmlEscaper in model MJadwalPpdb

--- app/Models/MJadwalPpdb.js
+++ app/Models/MJadwalPpdb.js
@@ -2,12 +2,15 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use("Model");
+const htmlEscaper = require("html-escaper");
 
 class MJadwalPpdb extends Model {
   static get table() {
     return "m_jadwal_ppdb";
   }
-
+  getKeterangan(keterangan) {
+    return keterangan ? htmlEscaper.unescape(keterangan) : "";
+  }
   soal() {
     return this.belongsTo("App/Models/MUjian");
   }
