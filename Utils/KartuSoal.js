d234db286a763ac6165dc048b87a10577277f57b
RaihanAdza <raihanvans@gmail.com>
2023-02-20 22:30:52 +0700

    update kartu soal

--- Utils/KartuSoal.js
+++ Utils/KartuSoal.js
@@ -1,7 +1,7 @@
 const htmlEscaper = require("html-escaper");
 const formattedHTML = (source) => {
     if(source.trim()){
-        return  `<div>${htmlEscaper.unescape(source)}</div>`
+        return  `<meta charset="UTF-8"><div>${htmlEscaper.unescape(source)}</div>`
     } else {
         return "<div>-</div>"
     }
