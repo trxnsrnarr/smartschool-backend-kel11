52ffd68eadc7a4df5c5369775bbe796d8979a8ab
RaihanAdza <raihanvans@gmail.com>
2023-02-22 06:50:19 +0700

    uji docs

--- start/routes.js
+++ start/routes.js
@@ -1991,6 +1991,7 @@ Route.get("/data-perpus", "UserController.getUserPerpus");
 Route.get("/data-perpus/:id", "UserController.showUserPerpus");
 
 Route.get("/utils/ujian/text", "UjianController.readingWordForExam");
+Route.post("/docs-import","DocsController.importDocs")
 
 Route.get("/ip", "MainController.ip");
 // wildcard (DROP AT BOTTOM OF THE FILE)
