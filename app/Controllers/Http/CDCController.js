7f89775c9f7ef08c05f47c4d85a7b49a4a4e7b6d
RaihanAdza <raihanvans@gmail.com>
2023-05-11 10:04:59 +0700

    post update anggota rombel

--- app/Controllers/Http/CDCController.js
+++ app/Controllers/Http/CDCController.js
@@ -5357,18 +5357,13 @@ class CDCController {
 
     // if (validation.fails()) {
     //   return response.unprocessableEntity(validation.messages());
-    // }
-
-    await Promise.all(
-      user_id.map(async (d) => {
-        await MAnggotaRombel.create({
-          m_rombel_id: rombel_id,
-          m_user_id: d,
-          dihapus: 0,
-          role: "anggota",
-        });
-      })
-    );
+    // }]
+    await MAnggotaRombel.create({
+      m_rombel_id: rombel_id,
+      m_user_id: user_id,
+      dihapus,
+      role:"anggota"
+    });
 
     return response.ok({
       message: messagePostSuccess,
