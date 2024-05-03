681e52f89b656ed64d6dcc5b26146fbd707c94ea
RaihanAdza <raihanvans@gmail.com>
2024-05-03 15:46:42 +0700

    fix ppdb Nomor

--- app/Controllers/Http/PPDBController.js
+++ app/Controllers/Http/PPDBController.js
@@ -3570,11 +3570,11 @@ class PPDBController {
           //     ? "02"
           //     : "03"
           // } - ${padNumber(urutan + 1, `${gelombang.diterima}`.length)}`;
-
+console.log(gelombangAktif?.gelombang?.nama);
           const nomorPeserta =
             sekolah?.id == 9487 || sekolah?.id == 9489
               ? `REG${
-                  gelombangAktif?.gelombang?.nama?.indexOf("SD") !== -1
+                  gelombangAktif?.toJSON()?.gelombang?.nama?.indexOf("SD") == -1
                     ? "SD"
                     : "MI"
                 }${padNumber(
